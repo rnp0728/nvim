9cff4cd8c4bbd1ecd3cd306d2261296027b57b55
rudra <rudra@sambuq.com>
2024-01-30 20:58:39 +0530

    removed wrong lazygit file

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -6,11 +6,4 @@ return {
     },
     lazy = false,
   },
-  {
-    "kdheepak/lazygit.nvim",
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
 }
