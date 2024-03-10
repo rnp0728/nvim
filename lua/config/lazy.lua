22ff04a17d2387374e5bc11763d5bcb39b968c2d
rudra <rudra@sambuq.com>
2024-03-10 08:29:26 +0530

    toggleterm & nvim-silicon implemented

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -6,4 +6,49 @@ return {
     },
     lazy = false,
   },
+  {
+    "mistricky/codesnap.nvim",
+    lazy = "true",
+    build = "make",
+    cmd = "CodeSnapPreviewOn",
+    opts = {
+      mac_window_bar = true, -- (Optional) MacOS style title bar switch
+      opacity = true, -- (Optional) The code snap has some opacity by default, set it to false for 100% opacity
+      watermark = "rnp", -- (Optional) you can custom your own watermark, but if you don't like it, just set it to ""
+      preview_title = "CodeSnap.nvim", -- (Optional) preview page title
+      editor_font_family = "CaskaydiaCove Nerd Font", -- (Optional) preview code font family
+      watermark_font_family = "Pacifico", -- (Optional) watermark font family
+    },
+    config = function(_, opts)
+      require("codesnap").setup(opts)
+    end,
+  },
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    config = function()
+      require("silicon").setup({
+        -- Configuration here, or leave empty to use defaults
+        font = "CaskaydiaCove Nerd Font Mono=34;Noto Color Emoji=34",
+        theme = "Dracula",
+        background = "#94E2D5",
+        window_title = function()
+          return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+        end,
+      })
+    end,
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    keys = {
+      {
+        "<c-\\>",
+        ":ToggleTerm direction='float' terminal path=%:p:h:<cr>",
+        desc = "ToggleTerm",
+      },
+    },
+  },
 }
